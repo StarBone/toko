2bfaf13298e93da034a9537a6257b8a8a18cf971
StarBone <anax.tore44@gmail.com>
2024-04-24 07:00:15 +0700

    hsn

--- function.php
+++ function.php
@@ -1,11 +1,33 @@
 <?php
     
+    session_start();
 
-    //
-    $c = mysqli_connect("localhost","root","","toko");
+    //connection
+    $con = mysqli_connect("localhost","root","","kasir");
 
-    if (!$c) {
-        echo "berhasil";
+    //login
+    if(isset($_POST["login"])){
+        //variable
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+
+        $check = mysqli_query($c, "SELECT * FROM user WHERE username = '$username' and password = '$password'");
+        $hitung = mysqli_num_rows($check);
+
+        if($hitung>0){
+            //jika data ditemukan maka berhasil login
+
+            $_SESSION['login'] = 'True';
+            header('location:index.php');
+        }
+        else {
+            //jika data tidak ditemukan
+
+            echo "<script>
+            alert('Username atau Password salah');
+            window.location.href='login.php'
+            </script>";
+        }
     }
 
 ?>
